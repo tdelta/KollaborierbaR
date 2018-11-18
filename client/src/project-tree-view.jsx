b5656ff9f24b042d07f290a31095d6d6d0b34ff7
Anton Haubner <anton.haubner@outlook.de>
2018-11-18 14:49:42 +0100

    Moved file tree nodes into its own component. Wrote documentation for it

--- client/src/project-tree-view.jsx
+++ client/src/project-tree-view.jsx
@@ -1,95 +1,13 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import FontAwesome from 'react-fontawesome';
 
-import FileIcon from './FileIcon.jsx';
+import FileNode from './file-node.jsx';
 
-class TreeNode extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            'visible': true
-        }
-    }
-
-    toggle() {
-        if (this.state.visible) {
-            this.setState({
-                'visible': false
-            });
-        }
-
-        else {
-            this.setState({
-                'visible': true
-            });
-        }
-    }
-
-    handleItemDoubleClick() {
-        if (this.props.data.type === 'file') {
-            this.props.onOpenFile(this.props.path);
-        }
-    }
-
-    render() {
-        if (this.props.data.hasOwnProperty('contents')) {
-            const visibility = {
-                'display': this.state.visible ?
-                    ''
-                    : 'none'
-            };
-
-            return (
-                <>
-                    <div onClick={() => {this.toggle();}}>
-                        <FileIcon
-                            className="projectTreeIcon"
-                            data={this.props.data}
-                            isOpen={this.state.visible}
-                        />
-                        {this.props.data.name}
-                    </div>
-                    <ul
-                        className="projectTreeList"
-                        style={visibility}
-                    >
-                        {
-                            this.props.data.contents.map((child) =>
-                                <li key={child.name}>
-                                    <TreeNode
-                                        data={child}
-                                        path={this.props.path.concat([child.name])}
-                                        onOpenFile={this.props.onOpenFile}
-                                    />
-                                </li>
-                            )
-                        }
-                    </ul>
-                </>
-            );
-        }
-
-        else {
-            return (
-                <div
-                    onDoubleClick={this.handleItemDoubleClick.bind(this)}
-                >
-                    <FileIcon
-                        className="projectTreeIcon"
-                        data={this.props.data}
-                        isOpen={this.state.visible}
-                    />
-                    {this.props.data.name}
-                </div>
-            );
-        }
-    }
-}
-
-class ProjectTreeView extends React.Component {
+export default class ProjectTreeView extends React.Component {
     render() {
         if (this.props.project.hasOwnProperty('contents')) {
             return (
@@ -103,7 +21,7 @@ class ProjectTreeView extends React.Component {
 
                     {
                         this.props.project.contents.map((item) =>
-                            <TreeNode
+                            <FileNode
                                 key={item.name}
                                 data={item}
                                 path={[item.name]}
@@ -121,4 +39,17 @@ class ProjectTreeView extends React.Component {
     }
 }
 
-export default ProjectTreeView;
+ProjectTreeView.propTypes = {
+    'onOpenFile': PropTypes.func,
+    'project': PropTypes.shape({
+        'contents': PropTypes.arrayOf(
+            PropTypes.instanceOf(FileNode)
+        ).isRequired,
+        'name': PropTypes.string.isRequired
+    }).isRequired
+};
+
+ProjectTreeView.defaultProps = {
+    'onOpenFile': () => {}
+};
+        
