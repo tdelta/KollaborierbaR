bc7b7050befad591d1b716982fe6e41f66db6038
Jonas Belouadi <bigjonas96@gmail.com>
2018-11-19 14:43:55 +0100

    State aus modal.js entfernt modal popped jetzt nicht mehr bei jedem render auf

--- client/src/modal.js
+++ client/src/modal.js
@@ -4,13 +4,6 @@ import React from 'react';
 import { ListGroup, ListGroupItem, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class ModalSelect extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal: true
-    };
-    this.toggle = this.toggle.bind(this);
-  }
 
     myClick() {
         alert("Hello World!");
@@ -24,27 +17,19 @@ class ModalSelect extends React.Component {
         }
         return <tbody>{rows}</tbody>;
     }
-  toggle() {
-    this.setState({
-      modal: !this.state.modal
-    });
-  }
-    componentWillReceiveProps(nextProps){
-        this.setState({modal: nextProps.isOpen});
-    }
   render() {
     return (
       <div>
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle}>Select Project</ModalHeader>
+        <Modal isOpen={this.props.isOpen} toggle={this.props.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.props.toggle}>Select Project</ModalHeader>
           <ModalBody>
         <ListGroup>
             {this.myAss()}
         </ListGroup>
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Select</Button>{' '}
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+            <Button color="primary" onClick={this.props.toggle}>Select</Button>{' '}
+            <Button color="secondary" onClick={this.props.toggle}>Cancel</Button>
           </ModalFooter>
         </Modal>
       </div>
