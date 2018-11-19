bf86dcdf23f76f416779db26d57d95cd1c03b52a
Marc Arnold <hobbit1996@gmx.net>
2018-11-19 11:16:53 +0100

    Added documentation for item, fileitem, folderitem

--- server/src/main/java/projectmanagement/FileItem.java
+++ server/src/main/java/projectmanagement/FileItem.java
@@ -1,15 +1,22 @@
 package projectmanagement;
 
+/**
+ * This class is a data container for Java Spring Marshalling.
+ * 
+ * 
+ * @author David Heck, Marc Arnold
+ *
+ */
 public class FileItem extends Item{
 
+	/**
+	 * This is the default constructor for the class.
+	 * 
+	 * @param name for the item
+	 */
     public FileItem(String name){
     
         super(name);
         Typ = "file";
     }
-
-    public String getTyp(){
-        return this.Typ;
-    }
-
 }
--- server/src/main/java/projectmanagement/Item.java
+++ server/src/main/java/projectmanagement/Item.java
@@ -1,22 +1,48 @@
 package projectmanagement;
 
+/**
+ * This class is a data container for Java Springs Marshalling. There are 
+ * two types of Items: folder and files. For both there are additional classes
+ * which extend this one.
+ * 
+ * @author David Heck, Marc Arnold
+ *
+ */
 public class Item{
 
     private String name;
     protected String Typ;
 
+    
+    /**
+     * This is the default constructor for the class
+     * 
+     * @param name of the item
+     */
     public Item(String name){
 
         this.name = name;
 
     }
 
+    /**
+     * 
+     * Basic getter of the name.
+     * 
+     * @return name of the item
+     */
     public String getName(){
 
         return this.name;
 
     }
 
+    /**
+     * 
+     * Basic getter of the Typ
+     * 
+     * @return the type of the item (folder/file) 
+     */
     public String getTyp(){
 
         return this.Typ;
