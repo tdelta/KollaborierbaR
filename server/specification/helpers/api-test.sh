3390ba67d1d695a72d43222756a055410ab66067
Anton Haubner <anton.haubner@outlook.de>
2018-11-28 20:39:53 +0100

    Removed dependency on curl from server api tests, since Build server is not guaranteed to have curl installed. Using bash script and /dev/tcp sockets instead.

--- server/specification/helpers/api-test.sh
+++ server/specification/helpers/api-test.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# This script essentially does the following:
+#
+# * build the server
+# * run the server
+# * wait until its ready
+# * run the api tests
+# * stop the server
+#
+# (There are some additional safety checks to avoid waiting forever.
+#  It depends on the server reporting its ready status on /actuator/health
+#  as "{"status":"UP"}"  (exact string match) )
+
 echo "Preparing tests on server api..."
 
 # Ensure server is already built
@@ -13,7 +25,8 @@ serverPid=$!
 
 # Busy waiting until server is ready
 echo "Waiting for server to be ready..."
-until [ "$(curl -s localhost:8080/actuator/health)" = "{\"status\":\"UP\"}" ]; do
+tryCounter=0
+until [ "$(helpers/get.sh localhost 8080 /actuator/health 2> /dev/null | tail -n1)" = "{\"status\":\"UP\"}" ]; do
     # Exit immediately, if it died
     kill -0 $serverPid
     serverDead=$?
@@ -23,6 +36,17 @@ until [ "$(curl -s localhost:8080/actuator/health)" = "{\"status\":\"UP\"}" ]; d
         echo "Server died, before api tests could start!"
         exit 1
     fi
+
+    # Exit, if we tried more than 60s
+    if [ $tryCounter -ge 60 ]
+    then
+        echo "Waiting for server more than a minute. Aborting."
+        kill -9 $serverPid
+        exit 1
+    fi
+
+    ((tryCounter++))
+
     sleep 1
 done
 
