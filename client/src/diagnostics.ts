45055c1a3b8465e9dd987aa2cca585db9c2a1d64
Martin <martin_x@live.de>
2018-12-01 22:56:08 +0100

    Converted editor to typescript and uncoupled it from the sidebar implementation

--- client/src/diagnostics.ts
+++ client/src/diagnostics.ts
@@ -4,7 +4,7 @@ enum Kind {
   NOTE = 'NOTE',
 }
 
-interface Diagnostic {
+export interface Diagnostic {
   message: string;
   column: number;
   line: number;
@@ -24,7 +24,7 @@ enum AnnotationType {
   info = 'info',
 }
 
-interface Annotation {
+export interface Annotation {
   row: number;
   column: number;
   text: string;
