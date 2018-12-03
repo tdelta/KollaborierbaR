012951b6d264f159ca4a494934828d5b0b32ac7a
Marc Arnold <marcoliver.arnold@stud.tu-darmstadt.de>
2018-12-03 15:53:59 +0100

    Finished top transformation from jsx to tsx

--- client/src/components/top.tsx
+++ client/src/components/top.tsx
@@ -6,139 +6,150 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../index.css';
 
 import {
-    Navbar,
-    NavbarBrand,
-    Nav,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem } from 'reactstrap';
+  Navbar,
+  NavbarBrand,
+  Nav,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
 
 import ModalSelect from '../modal.js';
 
 export default class Top extends React.Component<Props, State> {
+  private fileSelector: RefObject<HTMLInputElement>;
+  private downloadSelector: RefObject<HTMLInputElement>;
+  private fileReader?: FileReader;
 
-    private fileSelector: RefObject<HTMLInputElement>;
-    private downloadSelector: RefObject<HTMLInputElement>;
-    private fileReader?: FileReader;
+  constructor(props: Props) {
+    super(props);
+    this.fileSelector = React.createRef();
+    this.downloadSelector = React.createRef();
+    this.onFileChosen = this.onFileChosen.bind(this);
+    this.onFileLoaded = this.onFileLoaded.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.openProjectOnClick = this.openProjectOnClick.bind(this);
+    this.openFileOnClick = this.openFileOnClick.bind(this);
+    this.downloadFileOnClick = this.downloadFileOnClick.bind(this);
+    this.state = {
+      showModal: false,
+    };
+  }
 
-    constructor(props: Props) {
-        super(props);
-        this.fileSelector = React.createRef();
-        this.downloadSelector = React.createRef();
-        this.onFileChosen = this.onFileChosen.bind(this);
-        this.onFileLoaded = this.onFileLoaded.bind(this);
-        this.toggleModal = this.toggleModal.bind(this);
-        this.state = {
-            showModal: false
-        };
+  private toggleModal(): void {
+    this.setState({ showModal: !this.state.showModal });
+  }
+
+  private onFileChosen(event: HTMLInputEvent): void {
+    this.fileReader = new FileReader();
+    this.fileReader.onloadend = this.onFileLoaded;
+
+    if (event.target != null && event.target.files != null) {
+      this.fileReader.readAsText(event.target.files[0]);
     }
+  }
 
-    toggleModal(): void {
-        this.setState({ showModal: !this.state.showModal });
+  private onFileLoaded(): void {
+    if (this.fileReader != null && typeof this.fileReader.result === 'string') {
+      this.props.setText(this.fileReader.result);
     }
-     
-    onFileChosen(event: HTMLInputEvent): void{
-        this.fileReader = new FileReader();
-        this.fileReader.onloadend = this.onFileLoaded;
+  }
 
-        if(event.target != null && event.target.files != null){
-            this.fileReader.readAsText(event.target.files[0]);
-        }
+  private openProjectOnClick(): void {
+    if (this.fileSelector.current != null) {
+      this.fileSelector.current.click();
     }
+  }
 
-    onFileLoaded(): void {
-        if (this.fileReader != null && typeof this.fileReader.result === 'string'){
-            this.props.setText(this.fileReader.result);
-        }
+  private downloadFileOnClick(): void {
+    if (this.downloadSelector.current != null) {
+      this.downloadSelector.current.click();
     }
+  }
 
-    render() {
-        return(
-            <div>
-                <Navbar color="dark" dark expand="md">
-                    <NavbarBrand href="/">KollaborierbaR</NavbarBrand>
-                    <Nav className="ml-auto" navbar>
-                        <UncontrolledDropdown >
-                            <DropdownToggle nav caret>
-                                Project
-                            </DropdownToggle>
-                            <DropdownMenu right>
-                                <DropdownItem onClick={this.toggleModal}>
-                                    Open project
-                                </DropdownItem>
-                                <DropdownItem onClick={() =>{
-                                                            if(this.fileSelector.current != null){
-                                                                this.fileSelector.current.click();
-                                                            }
-                                                            }
-                                                      }>
-                                    Create project
-                                </DropdownItem>
-                            </DropdownMenu>
-                        </UncontrolledDropdown>
-                        <ModalSelect
-                            isOpen={this.state.showModal}
-                            toggle={this.toggleModal}
-                            setStructure={this.props.showProject}
-                        />
-                        <UncontrolledDropdown>
-                            <DropdownToggle nav caret>
-                                File
-                            </DropdownToggle>
-                            <DropdownMenu right>
-                                <DropdownItem onClick={() =>{
-                                                            if(this.downloadSelector.current != null){
-                                                                this.downloadSelector.current.click();
-                                                            }
-                                                            }
-                                                      }>
-                                    Save
-                                </DropdownItem>
-                                <DropdownItem onClick={() =>{
-                                                            if(this.fileSelector.current != null){
-                                                            this.fileSelector.current.click()
-                                                            }   
-                                                            }
-                                                      }>
-                                    Load
-                                </DropdownItem>
-                            </DropdownMenu>
-                        </UncontrolledDropdown>
-                    </Nav>
-                </Navbar>
-
-                <input
-                    type="file"
-                    id="file"
-                    ref={this.fileSelector}
-                    style={{display: 'none'}}
-                    onChange={this.onFileChosen}
-                />
-                
-                <a
-                    href={'data:text/plain;charset=utf-8,'+ encodeURIComponent(this.props.text)}
-                    download="test.txt"
-                >
-                    <input type="button" ref={this.downloadSelector} style={{display: 'none'}}/>
-                </a>
-            </div>
-        );
+  private openFileOnClick(): void {
+    if (this.fileSelector.current != null) {
+      this.fileSelector.current.click();
     }
+  }
+
+  public render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand href="/">KollaborierbaR</NavbarBrand>
+          <Nav className="ml-auto" navbar>
+            <UncontrolledDropdown>
+              <DropdownToggle nav caret>
+                Project
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem onClick={this.toggleModal}>
+                  Open project
+                </DropdownItem>
+                <DropdownItem onClick={this.openProjectOnClick}>
+                  Create project
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <ModalSelect
+              isOpen={this.state.showModal}
+              toggle={this.toggleModal}
+              setStructure={this.props.showProject}
+            />
+            <UncontrolledDropdown>
+              <DropdownToggle nav caret>
+                File
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem onClick={this.downloadFileOnClick}>
+                  Save
+                </DropdownItem>
+                <DropdownItem onClick={this.openFileOnClick}>Load</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Navbar>
+
+        <input
+          type="file"
+          id="file"
+          ref={this.fileSelector}
+          style={{ display: 'none' }}
+          onChange={this.onFileChosen}
+        />
+
+        <a
+          href={
+            'data:text/plain;charset=utf-8,$(encodeURIComponent(this.props.text))'
+          }
+          download="test.txt"
+        >
+          <input
+            type="button"
+            ref={this.downloadSelector}
+            style={{ display: 'none' }}
+          />
+        </a>
+      </div>
+    );
+  }
 }
+
 // defining HTMLInput Event
 interface HTMLInputEvent extends React.FormEvent<HTMLInputElement> {
-    target: HTMLInputElement & EventTarget;
+  target: HTMLInputElement & EventTarget;
 }
 
 // defining the strcuture of the state
-interface State{
-    showModal: boolean;
+interface State {
+  showModal: boolean;
 }
 
 // defining the structure of this react components properties
-interface Props{
-    text: string;
-    setText(text: string) : void;
-    showProject(project: object): void;
+interface Props {
+  text: string;
+  setText(text: string): void;
+  showProject(project: object): void;
 }
