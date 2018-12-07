97be31fcff06d77bd203aba969ee9e987f6da444
Martin <martin_x@live.de>
2018-12-07 16:32:37 +0100

    Fixed sliding annotations and removed javac from the implementation

--- server/src/main/java/linter/JavaCompilerLinter.java
+++ server/src/main/java/linter/JavaCompilerLinter.java
@@ -1,5 +1,14 @@
 package linter;
 
+import java.util.Hashtable;
+import java.io.IOException;
+
+import org.eclipse.jdt.core.compiler.IProblem;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,75 +28,95 @@ public class JavaCompilerLinter {
 
   /** Checks given source files for errors and returns diagnostics of all of them */
   public List<Diagnostic> check(final List<JavaFileObject> toCheck) {
-    // this collector allows to capture diagnostic data regarding the source code errors
-    final DiagnosticCollector<JavaFileObject> diagnosticsCollector =
-        new DiagnosticCollector<JavaFileObject>();
 
+    // Create a syntax parser
+    ASTParser parser = ASTParser.newParser(AST.JLS8);
+    parser.setResolveBindings(true);
+    try{
+        parser.setSource(toCheck.get(0).getCharContent(true).toString().toCharArray());
+    } catch (IOException e) {
+        parser.setSource(new char[0]);
+    }
+    // Settings:
+    // K_COMPIlATION_UNIT: Parse all expressions
+    // Bindings recovery: Create bindings between variables and their content
+    // Compiler options: For what to give errors
+    // UnitName: File name
+    parser.setKind(ASTParser.K_COMPILATION_UNIT);
+    parser.setBindingsRecovery(true);
+    parser.setCompilerOptions(getCompilerOptions());
+    parser.setUnitName(toCheck.get(0).getName());
+
+    // Compiler environment settings: from where to load resources
+    final String[] sources = {"[~/TODO]"};
+    final String[] classpath = {"[~/TODO]"};
+    parser.setEnvironment(classpath, sources, new String[] { "UTF-8"}, true); 
+
+    // Create a syntax tree
+    final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
     final JmlNotSupportedScanner scanner = new JmlNotSupportedScanner(toCheck.get(0));
 
-    // setup compilation task
-    final CompilationTask task =
-        compiler.getTask(
-            null, // no special output device, use System.err (will not be used, because of
-                  // diagnostics listener)
-            null, // use default file manager
-            diagnosticsCollector, // collect diagnostics instead of usual compiler output
-            Arrays.asList("-Xlint"), // enable all warnings
-            null, // no class names for additional annotation processing
-            toCheck // compile these files (can be located in memory,
-                    // @see linting.linter.java.JavaMemoryFileObject)
-            );
+    // Get errors for not supported features
+    List<Diagnostic> keyErrors = scanner.parseAst(cu);
 
-    // compile
-    task.call(); // WARNING: Result (success status) currently ignored
+    // Add all linter errors and warnings from jdt
+    Arrays.asList(cu.getProblems()).stream()
+        .map(problem -> normalizeDiagnostic(problem, toCheck.get(0)))
+        .forEach(d -> keyErrors.add(d));
 
-    // return diagnostics
-    List<Diagnostic> diagnostics = diagnosticsCollector
-        .getDiagnostics()
-        .stream()
-        .map(
-            JavaCompilerLinter
-                ::normalizeDiagnostic) // convert java diagnostics representation to our format,
-        // @see linting.linter.Diagnostics
-        .collect(Collectors.toList());
-
-    List<Diagnostic> keyErrors = scanner.getResults();
-
-    if(diagnostics != null){
-        if(keyErrors != null)
-            diagnostics.addAll(keyErrors);
-    }
-    else diagnostics = keyErrors;
-    return diagnostics;
+    return keyErrors;
   }
 
   /**
    * Converts java diagnostics representation (@see javax.tools.Diagnostic) to a simplified
    * representation (@see linting.linter.Diagnostic).
    */
-  private static Diagnostic normalizeDiagnostic(
-      final javax.tools.Diagnostic<? extends JavaFileObject> d) {
+  private static Diagnostic normalizeDiagnostic(final IProblem d, JavaFileObject source) {
     // determine the error type
     final Diagnostic.Kind kind;
 
-    switch (d.getKind()) {
-      case ERROR:
+    if(d.isError())
         kind = Diagnostic.Kind.ERROR;
-        break;
-
-      case WARNING:
+    else if(d.isWarning())
         kind = Diagnostic.Kind.WARNING;
-        break;
-
-      default: // to simplify things, everything else is interpreted as NOTE
+    else // to simplify things, everything else is interpreted as NOTE
         kind = Diagnostic.Kind.NOTE;
-    }
 
     return new Diagnostic(
-        d.getMessage(null),
-        d.getStartPosition(),
-        d.getEndPosition(),
-        d.getSource(),
+        d.getMessage(),
+        d.getSourceStart()+1,
+        d.getSourceEnd()+1,
+        source,
         kind);
   }
+
+  /**
+   * This method creates compiler options that will be set in the ASTParser to generate errors and warnings.
+   * @return A Hashtable containing the options
+   */
+  private Hashtable getCompilerOptions(){
+    Hashtable options = JavaCore.getDefaultOptions();
+    // Similar settings to javac -Xlint to recreate Xlint errors
+    options.put(JavaCore.COMPILER_PB_FALLTHROUGH_CASE,JavaCore.WARNING);
+    options.put(JavaCore.COMPILER_PB_AUTOBOXING,JavaCore.WARNING);
+    options.put(JavaCore.COMPILER_PB_DEPRECATION,JavaCore.WARNING);
+    options.put(JavaCore.COMPILER_PB_TERMINAL_DEPRECATION,JavaCore.WARNING);
+    options.put(JavaCore.COMPILER_PB_ENUM_IDENTIFIER,JavaCore.WARNING);
+    options.put(JavaCore.COMPILER_PB_FIELD_HIDING,JavaCore.WARNING);
+    options.put(JavaCore.COMPILER_PB_FINALLY_BLOCK_NOT_COMPLETING,JavaCore.WARNING);
+    options.put(JavaCore.COMPILER_PB_FORBIDDEN_REFERENCE,JavaCore.WARNING);
+    options.put(JavaCore.COMPILER_PB_INCOMPATIBLE_NON_INHERITED_INTERFACE_METHOD,JavaCore.WARNING);
+    options.put(JavaCore.COMPILER_PB_INCOMPLETE_ENUM_SWITCH,JavaCore.INFO);
+    options.put(JavaCore.COMPILER_PB_LOCAL_VARIABLE_HIDING,JavaCore.WARNING);
+    options.put(JavaCore.COMPILER_PB_MISSING_SERIAL_VERSION,JavaCore.WARNING);
+    options.put(JavaCore.COMPILER_PB_NO_EFFECT_ASSIGNMENT,JavaCore.INFO);
+    options.put(JavaCore.COMPILER_PB_NULL_REFERENCE,JavaCore.ERROR);
+    options.put(JavaCore.COMPILER_PB_POSSIBLE_ACCIDENTAL_BOOLEAN_ASSIGNMENT,JavaCore.WARNING);
+    options.put(JavaCore.COMPILER_PB_UNUSED_DECLARED_THROWN_EXCEPTION,JavaCore.WARNING);
+    options.put(JavaCore.COMPILER_PB_UNUSED_PARAMETER,JavaCore.WARNING);
+    // Set the java version to 1.8 so all features can be parsed
+    options.put(JavaCore.COMPILER_SOURCE,JavaCore.VERSION_1_8);
+    return options;
+  }
+
 }
