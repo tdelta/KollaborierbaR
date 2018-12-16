8f4a58d67d578756721fc28e13b7564650556c6b
Martin <martin_x@live.de>
2018-12-16 10:21:26 +0100

    Implemented a relay server in spring

---
+++ server/src/main/java/synchronization/CustomHandshakeHandler.java
@@ -0,0 +1,23 @@
+package synchronization;
+
+import synchronization.StompPrincipal;
+import java.security.Principal;
+import java.util.Map;
+import java.util.UUID;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+public class CustomHandshakeHandler extends DefaultHandshakeHandler{
+
+  /**
+    * Handles the websocket handshake when a user connects and returns an Object that identifies the user.
+    * This enables us to target users with specific messages
+    */
+  @Override
+  protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
+    // Generate principal with UUID as name
+    Principal stompPrincipal = new StompPrincipal(UUID.randomUUID().toString());
+    return stompPrincipal;
+  }
+}
---
+++ server/src/main/java/synchronization/data/Char.java
@@ -0,0 +1,25 @@
+package synchronization.data;
+
+public class Char{
+
+    private Position[] position;
+    private int counter;
+    private String siteId;
+    private char value;
+
+    public Position[] getPosition(){
+        return position;
+    }
+
+    public int getCounter(){
+        return counter;
+    }
+
+    public String getSiteId(){
+        return siteId;
+    }
+
+    public char getValue(){
+        return value;
+    }
+}
