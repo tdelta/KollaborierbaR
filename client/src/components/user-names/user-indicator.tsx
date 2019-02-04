82ca81843e62a0f7bb18c6a18a812289ec51d95b
Jonas Belouadi <bigjonas96@gmail.com>
2019-02-04 16:38:32 +0100

    Changed icons to cute animals

--- client/src/components/user-names/user-indicator.tsx
+++ client/src/components/user-names/user-indicator.tsx
@@ -2,7 +2,12 @@ import { UncontrolledTooltip } from 'reactstrap';
 import React from 'react';
 import './animals.css';
 
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import FontAwesomeIcon from 'react-fontawesome';
+import stringHash from "string-hash";
+
 export default class UserIndicator extends React.Component<Props, {}> {
+
   constructor(props: Props) {
     super(props);
   }
@@ -12,10 +17,13 @@ export default class UserIndicator extends React.Component<Props, {}> {
       <>
         <span className="login-text" id={this.props.name}>
           <div
-            style={{ backgroundColor: this.props.color }}
+            style={{ backgroundColor: this.props.color, color: '#343a40' }}
             className={'circle'}
           >
-            {this.props.name.charAt(0)}
+                    <FontAwesomeIcon
+                        name={this.userIcons[(stringHash(this.props.name) % this.userIcons.length)]}
+                        style={{ margin: 'auto' }}
+                    />
           </div>
         </span>
         <UncontrolledTooltip
