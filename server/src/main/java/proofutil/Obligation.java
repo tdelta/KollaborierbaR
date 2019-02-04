c4394c3e868c7fd00f1178c98870b0edcc73ed89
Jonas Belouadi <jonas.belouadi@posteo.net>
2019-02-04 14:27:44 +0100

    Fixed id

--- server/src/main/java/proofutil/Obligation.java
+++ server/src/main/java/proofutil/Obligation.java
@@ -1,21 +1,19 @@
 package proofutil;
-import java.util.List;
-import java.util.ArrayList;
 
 /**
  * This class is used as a container for open goals
  * @author Jonas Belouadi
  */
 public class Obligation{
-    private int id;
+    private long id;
     private String sequent;
     
-    public Obligation(int id, String sequent) {
+    public Obligation(long id, String sequent) {
     	this.id = id;
     	this.sequent = sequent;
     }
 
-    public int getId(int id){
+    public long getId(){
         return id;
     }
 
