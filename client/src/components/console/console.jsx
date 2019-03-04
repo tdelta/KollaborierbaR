c664f602a0001316b229e7ce5d423d0246c219ea
Marc Arnold <marcoliver.arnold@stud.tu-darmstadt.de>
2019-03-04 15:30:25 +0100

    Finshed first final version of the console. (With pop up if Key throws an error)

--- client/src/components/console/console.jsx
+++ client/src/components/console/console.jsx
@@ -10,56 +10,17 @@ import './console.css';
 
 export default class Sidebar extends React.Component {
 
-    constructor(props){
-        super(props);
-
-        this.state = {
-            // whether the sidebar is currently hidden, or not
-            'collapsed': true
-        }
-    }
-
-    /**
-     * invert visibility of the sidebar by collapsing it, or
-     * restoring it.
-     */
-    toggle() {
-        this.setState({
-            'collapsed': !this.state.collapsed,
-            'textheight': 0,
-            'textwidth': 0
-        });
-
-        // Resize the div containing the editor depending wheter the console
-        // is collapsed or not
-        if(this.state.collapsed){
-            var editor = document.getElementById("editor");
-            editor.style.height = "66%";
-        } else{
-            var editor = document.getElementById("editor");
-            editor.style.height = "98%";
-        }
-
-        // Make other components recalculate their width (Im looking at you ace)
-        window.dispatchEvent(new Event('resize'));
-    }
-
-    componentDidMount(){
-        
-    }
-    
     render(){
 
         const genVisibilityString = (collapsed) => collapsed ?
         'none' : '';
 
-        
-        var consoleStyleModForRestore = {
-        'display': genVisibilityString(!this.state.collapsed)
+        let consoleStyleModForRestore = {
+        'display': genVisibilityString(this.props.consoleIsVisible)
         };  
 
-        var consoleStyleModForCollapse = {
-            'display': genVisibilityString(this.state.collapsed)
+        let consoleStyleModForCollapse = {
+            'display': genVisibilityString(!this.props.consoleIsVisible)
         }; 
 
         return(
@@ -67,7 +28,7 @@ export default class Sidebar extends React.Component {
             <div 
                 className = "consoleRestoreHandle"
                 style = {consoleStyleModForRestore}
-                onClick = {this.toggle.bind(this)}
+                onClick = {this.props.invertConsoleVisibility}
                 
             >
                 <FontAwesome
@@ -79,7 +40,7 @@ export default class Sidebar extends React.Component {
             <div 
                 className = "consoleRestoreHandle"
                 style = {consoleStyleModForCollapse}
-                onClick = {this.toggle.bind(this)}
+                onClick = {this.props.invertConsoleVisibility}
                 
             >
                 <FontAwesome
