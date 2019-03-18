eec920829bf9a568429aa9a11777fd66a93e769b
Jonas Belouadi <bigjonas96@gmail.com>
2019-03-18 19:52:40 +0100

    Beweisen durch Rechtsklick auf Methoden jetzt möglich

--- client/src/key/key.ts
+++ client/src/key/key.ts
@@ -16,6 +16,10 @@ export default class Key {
 
   private addNewConsoleMessage: (message: String) => void;
 
+  private contractRegex: RegExp = /normal_behaviour|exceptional_behaviour|normal_behavior|exceptional_behavior/g;
+  // Find method declarations in the current line 
+  // https://stackoverflow.com/questions/68633/regex-that-will-match-a-java-method-declarations
+  private methodRegex: RegExp = /^[ \t]*(?:(?:public|protected|private)\s+)?(?:(static|final|native|synchronized|abstract|threadsafe|transient|(?:<[?\w\[\] ,&]+>)|(?:<[^<]*<[?\w\[\] ,&]+>[^>]*>)|(?:<[^<]*<[^<]*<[?\w\[\] ,&]+>[^>]*>[^>]*>))\s+){0,}(?!return)\b([\w.]+)\b(?:|(?:<[?\w\[\] ,&]+>)|(?:<[^<]*<[?\w\[\] ,&]+>[^>]*>)|(?:<[^<]*<[^<]*<[?\w\[\] ,&]+>[^>]*>[^>]*>))((?:\[\]){0,})\s+\b\w+\b\s*\(\s*(?:\b([\w.]+)\b(?:|(?:<[?\w\[\] ,&]+>)|(?:<[^<]*<[?\w\[\] ,&]+>[^>]*>)|(?:<[^<]*<[^<]*<[?\w\[\] ,&]+>[^>]*>[^>]*>))((?:\[\]){0,})(\.\.\.)?\s+(\w+)\b(?![>\[])\s*(?:,\s+\b([\w.]+)\b(?:|(?:<[?\w\[\] ,&]+>)|(?:<[^<]*<[?\w\[\] ,&]+>[^>]*>)|(?:<[^<]*<[^<]*<[?\w\[\] ,&]+>[^>]*>[^>]*>))((?:\[\]){0,})(\.\.\.)?\s+(\w+)\b(?![>\[])\s*){0,})?\s*\)(?:\s*throws [\w.]+(\s*,\s*[\w.]+))?\s*(?:\{|;)[ \t]*$/;
   constructor(
     notificationSystem: RefObject<NotificationSystem.System>,
     setProvenObligations: (provenObligations: number[]) => void,
@@ -29,6 +33,9 @@ export default class Key {
     this.getFilePath = getFilePath;
     this.proveFile = this.proveFile.bind(this);
     this.proveObligation = this.proveObligation.bind(this);
+    this.isMethodDeclaration = this.isMethodDeclaration.bind(this);
+    this.getObligations = this.getObligations.bind(this);
+    this.getContractsForMethod = this.getContractsForMethod.bind(this);
     this.setProofResults = setProofResults;
     this.setOpenGoals = setOpenGoals;
 
@@ -130,18 +137,41 @@ export default class Key {
    * @result An array where the index of a line that contains proof obligations
    *    is set to the index of the last obligations in the line. For the other lines it is undefined
    */
-  public getObligations(lines: string[]): number[] {
-    let numObligations = 0;
-    const result: number[] = [];
-    for (let i = 0; i < lines.length; i += 1) {
-      // Find the start of all proof obligations in the current line
-      const regex: RegExp = /normal_behaviour|exceptional_behaviour|normal_behavior|exceptional_behavior/g;
-      const obligations: RegExpMatchArray | null = lines[i].match(regex);
-      if (obligations) {
-        numObligations += obligations.length;
-        result[i] = numObligations - 1;
+    public getObligations(lines: string[]): number[] {
+      let numObligations = 0;
+      const result: number[] = [];
+      for (let i = 0; i < lines.length; i += 1) {
+        // Find the start of all proof obligations in the current line
+        const obligations: RegExpMatchArray | null = lines[i].match(this.contractRegex);
+        if (obligations) {
+          numObligations += obligations.length;
+          result[i] = numObligations - 1;
+        }
       }
+      return result;
     }
-    return result;
-  }
+
+    public isMethodDeclaration(line: string): boolean {
+      return this.methodRegex.test(line)
+    }
+
+    public getContractsForMethod(lines: string[], row: number): number[] {
+      const result: number[] = [];
+      if (this.isMethodDeclaration(lines[row])) {
+        let numObligations = 0;
+        for (let i = 0; i < lines.length; i += 1) {
+          const obligations: RegExpMatchArray | null = lines[i].match(this.contractRegex);
+          if (obligations) {
+            numObligations += obligations.length;
+            result.push(numObligations - 1);
+          }
+          if (row == i) {
+              break;
+          } else if (this.isMethodDeclaration(lines[i])) {
+              result.length = 0;
+            }
+        }
+      }
+      return result;
+    }
 }
