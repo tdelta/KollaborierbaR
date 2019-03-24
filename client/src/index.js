c1091c9b2f390021c9eb75a7e50656b378cc29ea
Anton Haubner <anton.haubner@outlook.de>
2019-03-24 16:03:27 +0100

    Prettier für js aktiviert und mit eslint integriert. Prettier Formatierung durchgeführt.

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './collaborative/marker-colors.css'
+import './collaborative/marker-colors.css';
 
 import App from './components/app.jsx';
 
 // render the main component (app) in a div (`root`) inside index.html
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
--- client/src/linting.js
+++ client/src/linting.js
@@ -1,22 +1,21 @@
-import {serverAddress} from './constants.ts';
+import { serverAddress } from './constants.ts';
 
 function lint(name, sourceCode) {
-    var url = new URL(serverAddress+'/lint');
+  var url = new URL(serverAddress + '/lint');
 
-    const params = {'name': name};
+  const params = { name: name };
 
-    url.search = new URLSearchParams(params);
+  url.search = new URLSearchParams(params);
 
-    return fetch(url, {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'text/plain', //evtl java spezifisch stattdessen?
-        },
-        body: sourceCode
-    })
-        .then((response) => response.json());
+  return fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'text/plain', //evtl java spezifisch stattdessen?
+    },
+    body: sourceCode,
+  }).then(response => response.json());
 }
 
 export default lint;
