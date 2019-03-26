d51d2aaa4dce45f93955ffced0059e27a478b260
Anton Haubner <anton.haubner@outlook.de>
2019-03-26 16:05:25 +0100

    Documented ProofController methods and class

--- server/src/main/java/server/ProofController.java
+++ server/src/main/java/server/ProofController.java
@@ -29,9 +29,15 @@ import proofutil.ObligationResult;
 import proofutil.ProofResult;
 
 /**
- * Basic KeY stub, that tries to prove all contracts in a file
+ * Controller which provides access to KollaborierbaRs KeY functionalities. Those are:
+ *
+ * <p>- Running proofs on a file stored on the server - Temporarily storing proofs, such that other
+ * clients working on the same file can access them - Permanently storing a history of proof results
+ * for a proof obligation for each file This is utilized by the client, such that users can retrieve
+ * and review proofs at a later time.
  *
  * @author Jonas Belouadi
+ * @author Anton Haubner {@literal <anton.haubner@outlook.de>}
  */
 @RestController
 @CrossOrigin
@@ -41,13 +47,20 @@ public class ProofController {
   private ConcurrentHashMap<String, HashMap<Integer, List<ObligationResult>>> obligationResults =
       new ConcurrentHashMap<>();
 
+  /**
+   * Required, to send events between Spring controllers.
+   *
+   * <p>In this class, it is used to inform {@link synchronization.ProofSyncController} about
+   * changes to the proof result history and temporarily stored proof results.
+   */
   @Autowired private ApplicationEventPublisher applicationEventPublisher;
 
   /**
-   * Prove all Proof Obligations in a .java file or by index if a index is provided
+   * Try to prove all proof obligations in a .java file or by index if an index is provided
    *
-   * @param className the path to the file relative to the projects folder
-   * @param obligationIdxs the indices of the obligations to prove
+   * @param className class name of the file in which the proofs shall be run
+   * @param obligationIdxs the indices of the obligations to prove. Counted from top to bottom in
+   *     the corresponding Java source file
    * @return the proof results
    */
   @RequestMapping(value = "/**/{className}.java", method = RequestMethod.GET)
@@ -57,10 +70,11 @@ public class ProofController {
       @RequestParam("obligationIdxs") final Optional<List<Integer>> obligationIdxs,
       final HttpServletRequest request) {
     // Get the file path for the request resource
-    final String path =
-        ((String) request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE))
-            .substring(7);
+    final String path = extractPath(request);
+
+    // KeYWrapper provides KeY functionalities to this API controller
     final KeYWrapper key = new KeYWrapper(path);
+
     // prove by index if index is present. ternary operator can be replaced with ifPresentOrElse if
     // Java 9 is used or higher
     final ProofResult result =
@@ -72,6 +86,19 @@ public class ProofController {
     return new ResponseEntity<ProofResult>(result, HttpStatus.OK);
   }
 
+  /**
+   * Lists all obligation indices for the given file, for which there are saved proofs available on
+   * the server.
+   *
+   * <p>The returned indices can be used to retrieve saved proofs using for example {@link
+   * #getCurrentProof} or {@link getHistoricProof}.
+   *
+   * @param className Name of the class, for which we want to list obligation indices of saved
+   *     proofs.
+   * @param request Object describing the HTTP request, which triggered this method. Filled in by
+   *     Spring.
+   * @return indices of saved proof results, counted per file from top to bottom.
+   */
   @RequestMapping(value = "/**/{className}.java/obligation", method = RequestMethod.GET)
   public List<Integer> listSavedObligations(
       @PathVariable final String className, final HttpServletRequest request) {
@@ -83,6 +110,23 @@ public class ProofController {
         obligationResults.getOrDefault(projectFilePath, new HashMap<>()).keySet());
   }
 
+  /**
+   * Temporarily store the latest prove result for a proof. It is lost as soon as the server is shut
+   * down.
+   *
+   * <p>This feature is used by KollaborierbaR to always share the latest proof result by any
+   * developer with everyone working on the same file. Using this route, such a temporarily result
+   * can be uploaded.
+   *
+   * <p>This method will inform {@link synchronization.ProofSyncController} about the change.
+   *
+   * @param className Name of the class, for which we want to save a temporary proof result.
+   * @param obligationIdx Index of the proof obligation to which the uploaded result belongs,
+   *     counted from top to bottom in the given Java file.
+   * @param request Object describing the HTTP request, which triggered this method. Filled in by
+   *     Spring.
+   * @param obligationResult Proof result to be stored.
+   */
   @RequestMapping(
       value = "/**/{className}.java/obligation/{obligationIdx}/last",
       method = RequestMethod.POST)
@@ -122,6 +166,17 @@ public class ProofController {
     applicationEventPublisher.publishEvent(event);
   }
 
+  /**
+   * Retrieves the temporary proof result stored by {@link #uploadCurrentObligationResult}.
+   *
+   * @param className Name of the class, for which we want to retrieve a temporary proof result.
+   * @param obligationIdx Index of the proof obligation for which the latest temporary results shall
+   *     be retrieved, counted from top to bottom in the given Java file.
+   * @param request Object describing the HTTP request, which triggered this method. Filled in by
+   *     Spring.
+   * @return Temporarily stored proof result, or a NOT_FOUND (404) response, if there is no such
+   *     result stored.
+   */
   @RequestMapping(
       value = "/**/{className}.java/obligation/{obligationIdx}/last",
       method = RequestMethod.GET)
@@ -138,6 +193,20 @@ public class ProofController {
         .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
   }
 
+  /**
+   * Retrieves a proof result from the history. See class description for more information on the
+   * proof history feature.
+   *
+   * @param className Name of the class, for which we want to retrieve a proof result from the
+   *     history.
+   * @param obligationIdx Index of the proof obligation for which the result shall be retrieved,
+   *     counted from top to bottom in the given Java file.
+   * @param historyIdx unique history id of the saved proof. See {@link #getHistoryItems} for more
+   *     information about history ids.
+   * @param request Object describing the HTTP request, which triggered this method. Filled in by
+   *     Spring.
+   * @return Saved proof result, or a NOT_FOUND (404) response, if there is no such result stored.
+   */
   @RequestMapping(
       value = "/**/{className}.java/obligation/{obligationIdx}/history/{historyIdx}",
       method = RequestMethod.GET)
@@ -155,6 +224,21 @@ public class ProofController {
         .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
   }
 
+  /**
+   * Retrieves a list of ids of all proof results stored in the history. See class description for
+   * more information on the proof history feature.
+   *
+   * <p>These ids can be used to retrieve a saved proof result using {@link #getHistoricProof}.
+   *
+   * @param className Name of the class, for which we want to retrieve the list of ids
+   * @param obligationIdx Index of the proof obligation for which the history ids shall be
+   *     retrieved.
+   * @param request Object describing the HTTP request, which triggered this method. Filled in by
+   *     Spring.
+   * @return List of ids of saved proof results. The history id of a saved result is an indicator of
+   *     its age. Bigger ids mean the saved result is more recent. So sorting them by descending
+   *     order sorts them in order of their age, beginning with the newest saved proofs.
+   */
   @RequestMapping(
       value = "/**/{className}.java/obligation/{obligationIdx}/history",
       method = RequestMethod.GET)
@@ -178,6 +262,20 @@ public class ProofController {
     return result;
   }
 
+  /**
+   * Adds a proof result to the history of saved results. See class description for more information
+   * on the proof history feature.
+   *
+   * <p>This method will inform {@link synchronization.ProofSyncController} about the changed
+   * history.
+   *
+   * @param className Name of the class, for which we want to save a result
+   * @param obligationIdx Index of the proof obligation for which a proof result shall be saved.
+   * @param request Object describing the HTTP request, which triggered this method. Filled in by
+   *     Spring.
+   * @return history id of the saved proof, see {@link #getHistoryItems} for more information on
+   *     these ids.
+   */
   @RequestMapping(
       value = "/**/{className}.java/obligation/{obligationIdx}/history",
       method = RequestMethod.POST)
@@ -218,6 +316,7 @@ public class ProofController {
 
       obligationResultHistory.add(0, obligationResult);
     }
+
     obligationResultHistory.add(newHistoryIdx, obligationResult);
     previousObligations.put(obligationIdx, obligationResultHistory);
 
@@ -237,6 +336,22 @@ public class ProofController {
     return newHistoryIdx;
   }
 
+  /**
+   * Deletes a proof result from the history of saved results. See class description for more
+   * information on the proof history feature.
+   *
+   * <p>This method will inform {@link synchronization.ProofSyncController} about the change.
+   *
+   * @param className Name of the class, for which we want to delete a result
+   * @param obligationIdx Index of the proof obligation for which a proof result shall be deleted.
+   * @param historyIdx unique history id of the result which shall be deleted. See {@link
+   *     #getHistoryItems} for more information about history ids.
+   * @param request Object describing the HTTP request, which triggered this method. Filled in by
+   *     Spring.
+   * @return history id of the result to be deleted, see {@link #getHistoryItems} for more
+   *     information on these ids. If there is no result matching the parameters, NOT_FOUND (404)
+   *     will be returned.
+   */
   @RequestMapping(
       value = "/**/{className}.java/obligation/{obligationIdx}/history/{historyIdx}",
       method = RequestMethod.DELETE)
@@ -291,6 +406,17 @@ public class ProofController {
     }
   }
 
+  /**
+   * Helper method for loading a proof result saved to history. See class description for more
+   * information on the proof history feature.
+   *
+   * @param projectFilePath path to a Java source file, for which a result shall be retrieved. It
+   *     includes the project name as the root folder.
+   * @param obligationId Index of the proof obligation for which a proof result shall be retrieved.
+   * @param historyIdx unique history id of the result which shall be retrieved. See {@link
+   *     #getHistoryItems} for more information about history ids.
+   * @return A proof result, if one had been saved for the given parameters. Nothing otherwise.
+   */
   private Optional<ObligationResult> retrieveObligationResult(
       final String projectFilePath, final int obligationId, final int historyIdx) {
     final List<ObligationResult> obligationResultHistory =
@@ -323,10 +449,39 @@ public class ProofController {
     return maybeObligationResult;
   }
 
+  /**
+   * Since Spring does not decode arbitrary paths (`/**`) into method parameters for us, we have to
+   * extract them from the request object.
+   *
+   * <p>This method just provides a basic extraction of the whole path without any further
+   * processing. See {@link #decodePath} for a similiar method, which provides more information on
+   * the contents of the path.
+   *
+   * @param request Spring generated object, describing a request. Can be obtained by simply adding
+   *     the type to the parameter list of a method with a {@link RequestMapping}.
+   * @return The full path used in {@code request}, with the leading `/proof/` part removed.
+   */
+  private static String extractPath(final HttpServletRequest request) {
+    return ((String) request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE))
+        .substring(7); // remove `/proof/` from the start of the path.
+  }
+
+  /**
+   * Simple data structure utilized by {@link #decodePath} to provide information extracted from a
+   * request path.
+   */
   private static class PathData {
+    /**
+     * Index of a proof obligation, as counted from top to bottom in its Java source file.
+     *
+     * <p>It may be set to {@code -1}, if there was no obligation index specified within the path.
+     */
     public final int obligationId;
+    /** Name of the project referenced in the request path. */
     public final String projectName;
+    /** Path to a file within the project */
     public final String filePath;
+    /** {@link projectName} and {@link filePath} concatenated with an `/` */
     public final String projectFilePath;
 
     public PathData(
@@ -341,26 +496,38 @@ public class ProofController {
     }
   }
 
+  /**
+   * Since Spring does not decode arbitrary paths (`/**`) into method parameters for us, we have to
+   * extract them from the request object.
+   *
+   * <p>This method is similiar to {@link extractPath}, but also extracts additional information
+   * from the path.
+   *
+   * <p>See {@link PathData} for the extracted information.
+   *
+   * @param request Spring generated object, describing a request. Can be obtained by simply adding
+   *     the type to the parameter list of a method with a {@link RequestMapping}.
+   * @return The full path used in {@code request}, with the leading `/proof/` part removed.
+   */
   private static PathData decodePath(final HttpServletRequest request) {
+    // regex, which is used to extract information from the request path by
+    // defining groups
     final String regex =
         "\\/proof\\/(?<ProjectName>[^\\/]+)\\/(?<Path>.+)\\/obligation(\\/(?<ObligationId>\\d+)\\/.+)?";
 
+    // Retrieving the request path from the request object
     final String input =
         (String) request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
-    System.out.println("ProofController: Prefix: " + input.substring(7));
-    System.out.println("ProofController: Matching " + input);
 
+    // Applying the regex
     final Pattern p = Pattern.compile(regex);
     final Matcher m = p.matcher(input);
 
-    System.out.println("ProofController: Matches: " + m.matches());
-
+    // Extracting information
     final String projectName = m.group("ProjectName");
-    System.out.println("ProofController: projectName " + projectName);
-
     final String path = m.group("Path");
-    System.out.println("ProofController: path " + path);
 
+    // If there was no obligation id specified in the path, set it to -1
     int obligationId = -1;
     try {
       obligationId = Integer.parseInt(m.group("ObligationId"));
@@ -370,10 +537,7 @@ public class ProofController {
           "ProofController: Could not identify obligation id. Might have been ommited to access ../obligation and is no error in that case.");
     }
 
-    System.out.println("ProofController: obligationId " + obligationId);
-
     final String projectFilePath = projectName + "/" + path;
-    System.out.println("ProofController: projectFilePath: " + projectFilePath);
 
     return new PathData(obligationId, projectName, path, projectFilePath);
   }
