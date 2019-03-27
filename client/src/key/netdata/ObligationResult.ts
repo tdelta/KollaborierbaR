a723760cee397f8f744794baaa2cbb449bdc2466
Anton Haubner <anton.haubner@outlook.de>
2019-03-27 17:09:59 +0100

    Documented remaining classes in key folder

--- client/src/key/netdata/ObligationResult.ts
+++ client/src/key/netdata/ObligationResult.ts
@@ -2,6 +2,15 @@ import ProofNode from '../prooftree/ProofNode';
 
 import OpenGoalInfo from './OpenGoalInfo';
 
+/**
+ * When running proofs on the backend server, it returns the results of a
+ * single proof attempt as a data structure, which conforms to this interface.
+ *
+ * See the documentation of the ObligationResult class of the backend server for more
+ * information on the members of this interface.
+ *
+ * Instances of this interface are usually stored as part of {@link ProofResults}
+ */
 export default interface ObligationResult {
   obligationIdx: number;
   targetName: string;
